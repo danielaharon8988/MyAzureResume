cd7521cfb43438327ffb338ecdba875112d871c1
Daniel Aharon <danielaharon8988@gmail.com>
2024-05-29 14:28:13 +0300

    deploy website

--- frontend/main.js
+++ frontend/main.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApiUrl = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://getresumecounterapp1.azurewebsites.net/api/GetResumeCounter?code=fcG2vyF0dzwR15Gk_w0UHs6Q-YoF7tHSNywdW_rdheKvAzFu-gdU_A%3D%3D';
 
 const getVisitCount = () => {
     let count = 30;
